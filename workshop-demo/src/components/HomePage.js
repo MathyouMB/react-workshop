3774b3bd95c98f21e12d7cf0b3b3952000070784
Matthew M-B <matthewmacraebovell@outlook.com>
2019-10-09 05:48:14 -0400

    fix grammar

--- workshop-demo/src/components/HomePage.js
+++ workshop-demo/src/components/HomePage.js
@@ -4,7 +4,7 @@ import '../style/home-page.scss';
 function HomePage() {
   return (
     <div className="home-page">
-       <h1>Yup, its the Home Page</h1>
+       <h1>Yup, its the Home Page.</h1>
     </div>
   );
 }
